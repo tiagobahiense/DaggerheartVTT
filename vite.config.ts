c941035cf6e31f53da15c62b985da727601d1fca
Tiago <tgbahiense@gmail.com>
2026-02-02 21:49:22 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Garanta que isso é uma barra simples, ou remova a linha.
+  build: {
+    outDir: 'dist',
+  }
 })
